b025169012c159cbec31ed193f66590a1125b8a6
luissoy <luisserranomarin@gmail.com>
2024-05-23 19:35:46 +0200

    Initial commit

---
+++ src/test/java/com/luissoy/mongocrudapi/MongoCrudApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luissoy.mongocrudapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoCrudApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
